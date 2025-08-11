eed968da3dff76fb9835fe4b4cb416f3fdeb25b1
JangAyeon <ayeon3052@ewhain.net>
2025-08-11 22:53:01 +0900

    refactor: 상태 분석 섹션 카드 분리

--- apps/web/@component/features/expense/statistics/cards/monthlyStateCard.tsx
+++ apps/web/@component/features/expense/statistics/cards/monthlyStateCard.tsx
@@ -1,14 +1,14 @@
 import { SpendingAnalysisResponse } from "@type/expense";
 import { FC } from "react";
 
-interface WeeklyStateCardProps {
+interface MonthlyStateCardProps {
   average: SpendingAnalysisResponse["averageSpending"];
   max: SpendingAnalysisResponse["maxSpending"];
   min: SpendingAnalysisResponse["minSpending"];
   comments: SpendingAnalysisResponse["insights"];
 }
 
-const WeeklyStateCard: FC<WeeklyStateCardProps> = ({
+const MonthlyStateCard: FC<MonthlyStateCardProps> = ({
   average,
   max,
   min,
@@ -18,21 +18,21 @@ const WeeklyStateCard: FC<WeeklyStateCardProps> = ({
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="bg-blue-50 rounded-lg p-4 col-span-1">
-          <p className="text-body-2 text-blue-700 mb-1">주간 평균</p>
+          <p className="text-body-2 text-blue-700 mb-1">월간 평균</p>
           <p className="text-title-2 font-bold">{average}원</p>
         </div>
 
         <div className="bg-pink-50 rounded-lg p-4 ">
-          <p className="text-body-2 text-pink-700 mb-1">주간 최대</p>
+          <p className="text-body-2 text-pink-700 mb-1">월간 최대</p>
           <p className="text-title-2 font-bold">{max}원</p>
         </div>
         <div className="bg-green-50 rounded-lg p-4">
-          <p className="text-body-2 text-green-700 mb-1">주간 최소</p>
+          <p className="text-body-2 text-green-700 mb-1">월간 최소</p>
           <p className="text-title-2 font-bold">{min}원</p>
         </div>
       </div>
       <div className="bg-gray-50 rounded-lg p-4">
-        <h4 className="text-body-2 font-medium mb-2">주간 추이 분석</h4>
+        <h4 className="text-body-2 font-medium mb-2">월간 추이 분석</h4>
         <div className="text-body-2 text-neutral-dark-gray">
           {comments.map((item, idx) => (
             <div key={idx}>* {item}</div>
@@ -43,4 +43,4 @@ const WeeklyStateCard: FC<WeeklyStateCardProps> = ({
   );
 };
 
-export default WeeklyStateCard;
+export default MonthlyStateCard;
