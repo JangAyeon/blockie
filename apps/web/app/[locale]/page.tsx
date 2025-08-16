3012df5f7b394e1a6b5ddf1096776aa2a59c91ef
JangAyeon <ayeon3052@ewhain.net>
2025-08-16 16:08:25 +0900

    fix: fix typescript props for build error

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,9 +1,9 @@
 import { pageUrl } from "@constant/page.route";
 import { redirect } from "@i18n/navigation";
-import { LocaleLayoutProps } from "@type/layout";
+import { LocaleParams } from "@type/layout";
 import { cookies } from "next/headers";
 
-const LocaleRootPage = async ({ params }: LocaleLayoutProps) => {
+const LocaleRootPage = async ({ params }: LocaleParams) => {
   const cookieStore = await cookies();
   const access_token = cookieStore.get("access_token")?.value ?? null;
   console.log("access_token", access_token);
