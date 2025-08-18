cebed2906b01fadae85dd1f300eeaacdab25419d
JangAyeon <ayeon3052@ewhain.net>
2025-08-18 22:32:52 +0900

    feat: web과 api 용 dockerFile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./@i18n/request.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  output: "standalone",
 };
 
 module.exports = withNextIntl(nextConfig);
