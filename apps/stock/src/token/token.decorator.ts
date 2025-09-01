25debfa001a1647ace01475903d8ed27ae8a9e9b
JangAyeon <ayeon3052@ewhain.net>
2025-09-01 18:09:34 +0900

    feat: 기본 swagger + 국내 주식 관련 endpoint 설정

---
+++ apps/stock/src/token/token.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+export const GetToken = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): string | null => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) return null;
+
+    const token = authHeader.replace('Bearer ', '');
+    return token;
+  },
+);
