ad2df4690c2e83536d8d358a190241e4d55b5b01
JangAyeon <ayeon3052@ewhain.net>
2025-09-02 23:58:24 +0900

    feat: 일봉/월봉/주봉, 실시간분봉 api

--- apps/stock/src/domestic/domestic.controller.ts
+++ apps/stock/src/domestic/domestic.controller.ts
@@ -1,13 +1,5 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Query,
-  UseGuards,
-  Sse,
-  MessageEvent,
-  Post,
-} from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Controller, Get, Param, Query, Post } from '@nestjs/common';
 import {
   ApiTags,
   ApiOperation,
@@ -15,7 +7,7 @@ import {
   ApiParam,
   ApiQuery,
 } from '@nestjs/swagger';
-
+import { DailyChartPeriod } from '@repo/types';
 import { DomesticService } from './domestic.service';
 // import { TokenAuthGuard } from 'src/token/token.guard';
 import { GetToken } from 'src/token/token.decorator';
@@ -56,61 +48,51 @@ export class DomesticController {
     return this.domesticService.getCurrentPrice(token, stockCode);
   }
 
-  @Get('chart/:stockCode')
+  @Get('daily/:ticker')
   @ApiOperation({
     summary: '주식 차트 데이터 조회',
-    description: '특정 종목의 일봉 차트 데이터를 조회합니다.',
+    description: '최근 30거래일(D), 30주(W), 30개월(M)의 과거 가격 데이터 제공',
   })
   @ApiParam({
-    name: 'stockCode',
+    name: 'ticker',
     description: '종목코드 (6자리)',
     example: '005930',
   })
   @ApiQuery({
     name: 'period',
-    description: '조회 기간 (일)',
-    example: 30,
-    required: false,
+    description: '조회 단위 (일/주/월)',
+    enum: ['D', 'W', 'M'], // 하드코딩 가능
+    example: 'D',
   })
-  async getChart(
+  async getDailyChart(
     @GetToken() token: string,
-    @Param('stockCode') stockCode: string,
-    @Query('period') period: number = 30,
+    @Param('ticker') ticker: string,
+    @Query('period') period: DailyChartPeriod = 'D',
   ) {
-    return this.domesticService.getDailyChart(token, stockCode, period);
+    return this.domesticService.getDailyChart(token, ticker, period);
   }
-
-  //   @Sse('realtime/:stockCode')
-  //   @ApiOperation({
-  //     summary: '주식 실시간 데이터 구독',
-  //     description: '특정 종목의 실시간 데이터를 SSE로 구독합니다.',
-  //   })
-  //   @ApiParam({
-  //     name: 'stockCode',
-  //     description: '종목코드 (6자리)',
-  //     example: '005930',
-  //   })
-  //   subscribeRealtime(
-  //     // @getUser() user: AuthUser,
-  //     @Param('stockCode') stockCode: string,
-  //   ): Observable<MessageEvent> {
-  //     return new Observable((observer) => {
-  //       const callback = (data: any) => {
-  //         observer.next({
-  //           data: JSON.stringify({
-  //             stockCode,
-  //             timestamp: new Date().toISOString(),
-  //             ...data,
-  //           }),
-  //         } as MessageEvent);
-  //       };
-
-  //       this.domesticWebsocketService.subscribeToStock(stockCode, callback);
-
-  //       // 클린업
-  //       return () => {
-  //         this.domesticWebsocketService.unsubscribeFromStock(stockCode, callback);
-  //       };
-  //     });
-  //   }
+  @Get('time/:ticker')
+  @ApiOperation({
+    summary: '주식당일분봉조회',
+    description:
+      '당일 분봉 데이터 (FID_INPUT_HOUR_1 에 미래일시 입력 시에 현재가로 조회)',
+  })
+  @ApiParam({
+    name: 'ticker',
+    description: '종목코드 (6자리)',
+    example: '005930',
+  })
+  @ApiQuery({
+    name: 'inqr_start_dt',
+    description:
+      '조회 시작일자(HHMMSS): "123000" 입력 시 12시 30분 이전부터 1분 간격으로 조회',
+    example: '090000',
+  })
+  async getTimeChart(
+    @GetToken() token: string,
+    @Param('ticker') ticker: string,
+    @Query('inqr_start_dt') inqr_start_dt: string,
+  ) {
+    return this.domesticService.getTimeChart(token, ticker, inqr_start_dt);
+  }
 }
--- apps/stock/src/domestic/domestic.service.ts
+++ apps/stock/src/domestic/domestic.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import {
+  DailyChartPeriod,
+  KISAccessTokenResponse,
+  StockPriceResponse,
+  StockDailyResponse,
+  StockTimeResponse,
+} from '@repo/types';
 
 @Injectable()
 export class DomesticService {
   private readonly logger = new Logger(DomesticService.name);
   private readonly baseUrl = 'https://openapi.koreainvestment.com:9443';
   private accessToken: string;
-  // private readonly appkey = 'PSUVoLDlZYICMlol7mN0QWmMj4vChD6BhVI2';
-  // private readonly appsecret =
-  //   'KRR5Cq+TsYWztpQJHgxh/l0OzMYO07n6HDz5CznLd32DhZfxEuHJA+pn4pczaZZyUNgmbijR9OTXnjKDjenWjnTWtyZglHYrIgMet0m2GN5T6s4VMG66BHP9/QrlGGk/SwGpyDVu506xtHe3ykMtycpa9sLIfBJ8Z2IS1fEpG3FVyKFw+fs=';
 
   constructor(
     private readonly httpService: HttpService,
@@ -17,7 +21,7 @@ export class DomesticService {
   ) {}
 
   // 액세스 토큰 발급
-  async getAccessToken(): Promise<void> {
+  async getAccessToken(): Promise<KISAccessTokenResponse> {
     // if (this.accessToken) return this.accessToken;
 
     try {
@@ -44,7 +48,10 @@ export class DomesticService {
   }
 
   // 현재가 조회
-  async getCurrentPrice(token: string, stockCode: string) {
+  async getCurrentPrice(
+    token: string,
+    stockCode: string,
+  ): Promise<StockPriceResponse> {
     // const token = await this.getAccessToken();
     // // const token = this.accessToken;
     console.log('getCurrentPrice token: ', token, ' stockCode: ', stockCode);
@@ -61,7 +68,7 @@ export class DomesticService {
             custtype: 'P',
           },
           params: {
-            FID_COND_MRKT_DIV_CODE: 'J',
+            FID_COND_MRKT_DIV_CODE: 'J' /* J:KRX, NX:NXT, UN:통합 */,
             FID_INPUT_ISCD: stockCode,
           },
         },
@@ -70,15 +77,24 @@ export class DomesticService {
       return response.data;
     } catch (error) {
       this.logger.error('현재가 조회 실패:', error);
-      //   throw error;
+      throw error;
     }
   }
 
-  // 일봉 차트 조회
-  async getDailyChart(token: string, stockCode: string, period: number = 30) {
-    // // const token = await this.getAccessToken();
-    // const token = this.accessToken;
-    console.log('getDailyChart token: ', token, ' stockCode: ', stockCode);
+  // 주식현재가 일자별
+  async getDailyChart(
+    token: string,
+    stockCode: string,
+    period: DailyChartPeriod,
+  ): Promise<StockDailyResponse> {
+    console.log(
+      'getDailyChart token: ',
+      token,
+      ' stockCode: ',
+      stockCode,
+      ' period:',
+      period,
+    );
 
     try {
       const response = await this.httpService.axiosRef.get(
@@ -92,10 +108,53 @@ export class DomesticService {
             tr_id: 'FHKST01010400',
           },
           params: {
-            FID_COND_MRKT_DIV_CODE: 'J',
+            FID_COND_MRKT_DIV_CODE: 'J' /* J:KRX, NX:NXT, UN:통합 */,
             FID_INPUT_ISCD: stockCode,
-            FID_PERIOD_DIV_CODE: 'D',
+            FID_PERIOD_DIV_CODE: period,
+            /* 기간분류코드: 
+            D : (일)최근 30거래일
+            W : (주)최근 30주
+            M : (월)최근 30개월 */
             FID_ORG_ADJ_PRC: '1',
+            /* 수정주가 원주가 가격:
+            0 : 수정주가미반영
+            1 : 수정주가반영
+            * 수정주가는 액면분할/액면병합 등 권리 발생 시 과거 시세를 현재 주가에 맞게 보정한 가격
+            */
+          },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      this.logger.error('일봉 차트 조회 실패:', error);
+      throw error;
+    }
+  }
+  // 국내주식기간별시세(일/주/월/년)
+  async getTimeChart(
+    token: string,
+    ticker: string,
+    inqr_start_dt: string,
+  ): Promise<StockTimeResponse> {
+    try {
+      const response = await this.httpService.axiosRef.get(
+        `${this.baseUrl}/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
+            appkey: this.configService.get('KIS_APP_KEY'),
+            appsecret: this.configService.get('KIS_APP_SECRET'),
+            tr_id: 'FHKST03010200',
+            custtype: 'P',
+          },
+          params: {
+            FID_COND_MRKT_DIV_CODE: 'J' /* J:KRX, NX:NXT, UN:통합 */,
+            FID_INPUT_ISCD: `${ticker}`,
+            FID_INPUT_HOUR_1: `${inqr_start_dt}`,
+            FID_PW_DATA_INCU_YN: `Y`,
+            FID_ETC_CLS_CODE: '00',
           },
         },
       );
